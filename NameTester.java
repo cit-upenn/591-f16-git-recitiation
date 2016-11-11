5c60a5730a938123291806f2c4ede93acf03a992
Donna <mujinyun2009@gmail.com>
2016-11-11 16:52:52 -0500

    Modify name tester

--- NameTester.java
+++ NameTester.java
@@ -7,8 +7,7 @@ public class NameTester {
 	public static void main(String[] args) {
 		Name name = new Name("Mr.", "Bob", "Manfred", "Jinkens", "Ron", "O'Brien", "Jinkens", "III", "Ph.D.");
 		System.out.printf("%s %s %s %s (formerly %s), %s %s, %s\n", name.getTitle(), name.getFirst(), name.getMiddle(),
-				name.getSurname(), name.getMaternal(), name.getPaternal(), name.getSuffixGenerational(),
-				name.getSuffixProfessional());
+				name.getSurname(), name.getMaternal(), name.getPaternal(), name.getSuffixGenerational());
 	
 	}
 }
